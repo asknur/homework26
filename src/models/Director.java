4c69ecdf7696b666e80f77bfe7c8500e5d96c313
Askar <nurbekovaskar099@gmail.com>
2025-04-21 01:26:29 +0600

    Created class for director field and added getters and setters

---
+++ src/models/Director.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Director {
+    private String fullname;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+}
