0056077916f074cd0ecc7d341753a5addade5681
Askar <nurbekovaskar099@gmail.com>
2025-04-21 20:38:29 +0600

    Created class Film for reading file json

---
+++ src/models/Film.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.List;
+
+public class Film {
+    private String name;
+    private int year;
+    private String description;
+    private Director director;
+    private List<Cast> cast;
+
+    public Film(String name, int year, String description, Director director, List<Cast> cast) {
+        this.name = name;
+        this.year = year;
+        this.description = description;
+        this.director = director;
+        this.cast = cast;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public List<Cast> getCast() {
+        return cast;
+    }
+
+    public void setCast(List<Cast> cast) {
+        this.cast = cast;
+    }
+
+}
