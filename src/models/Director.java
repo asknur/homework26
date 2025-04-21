7fa479dab05b8aa336c4c196a9f9877ca469ac5c
Askar <nurbekovaskar099@gmail.com>
2025-04-21 20:37:18 +0600

    Realised method toString for mathing name of director

--- src/models/Director.java
+++ src/models/Director.java
@@ -1,13 +1,18 @@
 package models;
 
 public class Director {
-    private String fullname;
+    private String fullName;
 
     public String getFullname() {
-        return fullname;
+        return fullName;
     }
 
     public void setFullname(String fullname) {
-        this.fullname = fullname;
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return fullName;
     }
 }
