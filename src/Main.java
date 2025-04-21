34773557b7e3cbfbd7f40f44bbfebd76e5d344d0
Askar <nurbekovaskar099@gmail.com>
2025-04-21 20:45:05 +0600

    Realised some aperations to match the result of reading json file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import com.google.gson.JsonObject;
+import models.Film;
+import util.UtilFilms;
+
 public class Main {
     public static void main(String[] args) {
-
+        Film[] films = UtilFilms.readFilms();
+        for (Film film : films) {
+            System.out.println("Название: " + film.getName());
+            System.out.println("Год выпуска: " + film.getYear());
+            System.out.println("Тип: " + film.getDescription());
+            System.out.println("Режиссер: " + film.getDirector());
+            System.out.println("Актеры:" + film.getCast());
+            System.out.println("----------------------");
+        }
     }
 }
